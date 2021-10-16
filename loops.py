de7b093adfd3e7cd97a54a50606016c8ec33b6c1
Roman Anojan Figurado <91520127+RomanFigurado@users.noreply.github.com>
2021-10-16 16:27:05 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,20 @@
+i = 1
+while i < 8:
+
+  print(i)
+  i = i + 1
+print("==========")
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+  print("==========")
+  i = 0
+  while i < 6:
+    i += 1
+    if i == 3:
+      continue
+    print(i)
